45522bda6cf3e6c57e9aab13d0edb3bba1c6c70d
DhanuM-2020 <m19dhanu@gmail.com>
2023-02-24 22:01:21 +0530

    SOL2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,6 +5,9 @@ public class Main {
         public int product(int x,int y,int z){
             return x*y*z;
         }
+        public int product(int x,int y){
+            return x*y;
+        }
         public double product(double x,double y){
             return x*y;
         }
@@ -12,6 +15,7 @@ public class Main {
     }
     public static void main(String[] args){
         Product p=new Product();
+        p.product(2,4);
         p.product(2,4,8);
         p.product(6.00,7.00);
 
